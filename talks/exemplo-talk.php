923c8ca984297a5557be1714db0de90fdc1544fa
Vinicius Lage <op.vini@gmail.com>
2016-03-08 01:08:39 -0300

    Added files via upload

---
+++ talks/exemplo-talk.php
@@ -0,0 +1,31 @@
+<?php
+
+
+class ExemploTalk extends MinTalk
+{
+
+  public $talk = array(
+  
+	'pt-br' => array(
+	  0 => "Esse é o erro zero",
+	  1 => "Esse é o erro um",
+	  2 => "Esse é o erro dois",
+	  3 => "Esse é o erro três",
+	  4 => "Esse é o erro quatro"
+	),
+	
+	'en' => array(
+	  0 => "This is the error zero",
+	  1 => "This is the error one",
+	  2 => "This is the error two",
+	  3 => "This is the error three",
+	  4 => "This is the error four"
+	)
+	
+  );
+  
+
+}
+
+
+?>
---
+++ talks/login-talk.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class LoginTalk extends MinTalk
+{
+
+  public $talk = array(
+  
+	'pt-br' => array(
+	  0 => "Login efetuado com sucesso.",
+	  1 => "Usuario ou senha invalidos.",
+	  2 => "Existe um erro em seus campos.",
+	  3 => "Logout efetuado com sucesso.",
+	  4 => "Token invalido.",
+	  5 => "Token nao informado."
+	),
+	
+	'en' => array(
+	  0 => "Login successfully.",
+	  1 => "Invalid username or password",
+	  2 => "Invalid fields.",
+	  3 => "Logout successfully.",
+	  4 => "Invalid token.",
+	  5 => "Token not set."
+	)
+	
+  );
+  
+
+}
+
+
+?>
